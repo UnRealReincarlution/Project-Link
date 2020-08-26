9c6681d163b2a192c8b7e9114063d8324257d7e9
UnRealReincarlution <benwhite2413@gmail.com>
2020-08-26 18:52:03 +1200

    Update for API

--- js/script.js
+++ js/script.js
@@ -1,62 +1,50 @@
 // Testing and Development Script for the Link Project.
+// let team_1 = {
+//     name: 'The betrayors',
+//     member_count: 5,
+//     members: [{name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}],
+//     wins: 2,
+//     tournaments: ['adw8e94rr87', 'cyw8e94yr87', 'ckw8e24em57']
+// }
 
-let _1 = {
-    name: 'Friday Fights Tournament',
-    game: 'CS:GO',
-    time: new Date(2020, 7, 14),
-    more_info: `/tournaments?n=${this.game}`,
-    cost: 'Free',
-    platform: 'PC',
-    prize_pool: 'Discord Nitro',
-    id: 'cyw8e94yr87'
-};
+// let team_2 = {
+//     name: 'Honestly Borken',
+//     member_count: 5,
+//     members: [{name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}],
+//     wins: 1,
+//     tournaments: ['adw8e94rr87', 'cyw8e94yr87', 'ckw8e24em57']
+// }
 
-let _2 = {
-    name: 'Highly-Competitive Pool',
-    game: 'Valorant',
-    time: new Date(2020, 7, 16, 18),
-    more_info: `/tournaments?n=${this.game}`,
-    cost: '$2/Per Person',
-    platform: 'PC',
-    prize_pool: '$200+',
-    id: 'adw8e94rr87'
-};
+// let team_3 = {
+//     name: 'Awaiting Success',
+//     member_count: 4,
+//     members: [{name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}],
+//     wins: 0,
+//     tournaments: ['adw8e94rr87', 'cyw8e94yr87', 'ckw8e24em57']
+// }
 
-let _3 = {
-    name: 'Less-Competitive Pool',
-    game: 'Valorant',
-    time: new Date(2020, 7, 18, 20),
-    more_info: `/tournaments?n=${this.game}`,
-    cost: '$1/Per Person',
-    platform: 'PC',
-    prize_pool: '$50+',
-    id: 'ckeee94yr87'
-};
+// let team_4 = {
+//     name: 'Edank noodles',
+//     member_count: 5,
+//     members: [{name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}],
+//     wins: 4,
+//     tournaments: ['adw8e94rr87', 'cyw8e94yr87', 'ckw8e24em57']
+// }
 
-let _4 = {
-    name: 'Friday Fights Tournament',
-    game: 'Rainbow Six Siege',
-    time: new Date(2020, 7, 21, 17),
-    more_info: `/tournaments?n=${this.game}`,
-    cost: 'Free',
-    platform: 'PC',
-    prize_pool: 'Discord Nitro',
-    id: 'ckw8e24em57'
-};
+// createTournament(_1);
+// createTournament(_2);
+// createTournament(_3);
+// createTournament(_4);
 
-let team = {
-    name: 'The betrayors',
-    member_count: 5,
-    members: [{name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}, {name: 'Edank', information: []}],
-    wins: 2,
-    tournaments: ['adw8e94rr87', 'cyw8e94yr87', 'ckw8e24em57']
-}
+// createTeam(team_4);
+// createTeam(team_1);
+// createTeam(team_2);
+// createTeam(team_3);
 
-createTournament(_1);
-createTournament(_2);
-createTournament(_3);
-createTournament(_4);
+// https://challonge.com/communities/5a986eb3b5268547f1c52791/tournaments
 
-tm().login();
-tm('#tournaments').displayTournaments();
-tm('#teams').displayTeams();
+// let response = $.get("https://challonge.com/communities/5a986eb3b5268547f1c52791/tournaments", { api_key: KEY} ).done(function() {
+//     console.log(response);
+// });    
+
+
--- js/tournament.js
+++ js/tournament.js
@@ -4,10 +4,19 @@
  */
 
 let tournaments = [];
+let teams = [];
 
 class Tournament {
     constructor(...args) {
-        this.information = args[0];
+        this.teams = args[1];
+
+        args = args[0];
+
+        this.name = args.name;
+        this.url = `https://challonge.com/${args.url}`;
+        this.id = args.id;
+
+        this.information = args;
     }
 
     logSelf() {
@@ -47,14 +56,22 @@ class User {
     }
 
     login() {
-        if(localStorage.getItem('jaw9jd09q28eadwa') !== null){
-            let login_token = JSON.parse(localStorage.getItem('jaw9jd09q28eadwa'));
+        if(localStorage.getItem('jaw9jd09q28eadwa') !== ""){
+            // Login Information - Grab from Mongo or Firebase upon implementation.
+            let login_token = JSON.parse(localStorage.getItem('jaw9jd09q28eadwa')); 
             console.log("Logging In");
+
+            if(document.getElementById("home_page")){
+                document.getElementById('welcome_message').innerHTML = `Welcome ${login_token.username}`;
+                document.getElementById('welcome_sub').innerHTML = `Have a look around, signup for tournaments and have a good time!`;
+                document.getElementById('signup_href_main').classList.add('hidden');
+            }
 
             document.getElementById('user_logged_out').classList.add('hidden');
             document.getElementById('users_name').innerHTML = login_token.username;
         }else{
             document.getElementById('user_logged_in').classList.add('hidden');
+            document.getElementById('welcome_message').innerHTML = `Welcome Challenger`;
         }
     }
 }
@@ -63,15 +80,15 @@ const renderTournaments = (element_id) => {
     let return_value = document.createElement('div');
 
     tournaments.forEach((element, index) => {
-        element = element.information;
-
         let refrence_date = new Date();
 
         if (refrence_date < element.time) {
             refrence_date.setDate(refrence_date.getDate() + 1);
         }
 
-        let msec = element.time - refrence_date;
+        let this_date = new Date(element.information.start_at)
+
+        let msec = this_date - refrence_date;
         let hh =  Math.floor(msec / 1000 / 60 / 60);
         let unit = 'Hours';
 
@@ -82,23 +99,41 @@ const renderTournaments = (element_id) => {
             unit = 'Days'
         }
 
+        
+
         //console.log(element);
 
         let element_for_editing = document.createElement('div');
+            element_for_editing.setAttribute('onclick', `maximiseWindow("${element.id}", ${index})`);
             //element_for_editing.addEventListener('click', element.more_info);
+            element_for_editing.setAttribute('tournament-id', index);
             element_for_editing.classList.add('tournament');
+
+        let textify = document.createElement('div');
 
         let title = document.createElement('h4');
             title.innerHTML = element.name;
-            element_for_editing.append(title);
+            textify.append(title);
+
+        let desc = document.createElement("p");
+            desc.innerHTML = element.information.description;
+
+            desc.style.textAlign = 'left';
+            desc.style.margin = '0';
+
+            desc.classList.add("tournament_desc");
+            desc.style.textOverflow = 'elipsis';
+
+            textify.append(desc);
+             element_for_editing.append(textify);
         
         let game = document.createElement('h6');
-            game.innerHTML = element.game;
+            game.innerHTML = element.information.game_name;
             element_for_editing.append(game);
         
-        let fee = document.createElement('h6');
-            fee.innerHTML = element.cost;
-            element_for_editing.append(fee);
+        let identification = document.createElement('h6');
+            identification.innerHTML = element.information.id;
+            element_for_editing.append(identification);
 
         let date = document.createElement('h5');
             date.innerHTML = `Starts in ${hh} ${unit}`;
@@ -106,6 +141,7 @@ const renderTournaments = (element_id) => {
 
         let button = document.createElement('div');
             button.classList.add('animation_button');
+            button.setAttribute('onclick', `openURL("${element.url}")`);
 
             let registered = document.createElement('button');
                 registered.innerHTML = `Register`;
@@ -129,13 +165,21 @@ const renderTournaments = (element_id) => {
 }
 
 const createTournament = (...tournamentArguments) => {
-    tournaments.push(new Tournament(tournamentArguments[0]));
+    tournaments.push(new Tournament(tournamentArguments[0], tournamentArguments[1]));
+
+    localStorage.setItem("T_CACHE", JSON.stringify(tournaments));
+}
+
+const openURL = (url) => {
+    window.open(url);
 }
 
 const renderTeams = () => {
   let return_value = document.createElement('div');
 
-    tournaments.forEach((element, index) => {
+  //console.log(teams)
+
+    teams.forEach((element, index) => {
         element = element.information;
 
         let refrence_date = new Date();
@@ -166,34 +210,36 @@ const renderTeams = () => {
             element_for_editing.append(title);
         
         let game = document.createElement('h6');
-            game.innerHTML = element.game;
+            game.innerHTML = `${element.member_count} / 5 Members`;
             element_for_editing.append(game);
         
         let fee = document.createElement('h6');
-            fee.innerHTML = element.cost;
+            fee.innerHTML = `${element.wins} Wins`;
             element_for_editing.append(fee);
 
-        let date = document.createElement('h5');
-            date.innerHTML = `Starts in ${hh} ${unit}`;
-            element_for_editing.append(date);
+        let applications = document.createElement('h5');
+            applications.innerHTML = (element.member_count < 5) ? 'OPEN' : 'FULL';
+            element_for_editing.append(applications);
 
-        let button = document.createElement('div');
-            button.classList.add('animation_button');
+        if(element.member_count < 5){
+            let button = document.createElement('div');
+                button.classList.add('animation_button');
 
-            let registered = document.createElement('button');
-                registered.innerHTML = `Register`;
-                button.append(registered);
-            
-            let register_anim = document.createElement('span');
-                register_anim.classList.add('offset_button');
-                button.append(register_anim);
-            
-            let registered_text = document.createElement('p');
-                registered_text.innerHTML = "Register";
-                registered_text.classList.add('offset_text');
-                button.append(registered_text);
+                let registered = document.createElement('button');
+                    registered.innerHTML = `Apply`;
+                    button.append(registered);
+                
+                let register_anim = document.createElement('span');
+                    register_anim.classList.add('offset_button');
+                    button.append(register_anim);
+                
+                let registered_text = document.createElement('p');
+                    registered_text.innerHTML = "Apply";
+                    registered_text.classList.add('offset_text');
+                    button.append(registered_text);
 
-            element_for_editing.append(button);
+                element_for_editing.append(button);
+        }
 
         return_value.append(element_for_editing); 
     });
@@ -201,8 +247,8 @@ const renderTeams = () => {
     return return_value;
 }
 
-const createTeam = () => {
-  tournaments.push(new Tournament(tournamentArguments[0]));
+const createTeam = (...teamArguments) => {
+  teams.push(new Team(teamArguments[0]));
 }
 
 const tm = (...args) => {
@@ -215,6 +261,7 @@ const tm = (...args) => {
 
             renderingLoc.displayTournaments = (...tournamentArgs) => {
                 let new_element = renderTournaments();
+                    
                 renderingLoc.append(new_element);
             }
 
@@ -232,3 +279,85 @@ const tm = (...args) => {
         return new User();
     }
 }
+
+const maximiseWindow = (id, index) => {
+    //$(`div[tournament-id='${index}']`).addClass("full_screen");
+    window.location.href = `./tournament.html?id=${id}&index=${index}`;
+}
+
+const displayMaximisedWindow = () => {
+    tournaments = JSON.parse(localStorage.getItem("T_CACHE"));
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    const tournament_id = urlParams.get('id');
+    const tournament_index = urlParams.get('index');
+
+    //console.log(tournament_id, tournament_index);
+    //console.log(tournaments);
+
+    let t_t = tournaments[tournament_index];
+
+    document.getElementById("tournament_header").innerHTML = t_t.name;
+    window.document.title = t_t.name;
+
+    console.log(t_t);
+
+    let information_div = document.createElement("div");
+
+    let first_half = document.createElement("div");
+        first_half.classList.add("tournament_flex")
+
+    let description = document.createElement("div");
+        description.innerHTML = t_t.information.description;
+        first_half.appendChild(description);
+    
+    let teams_count = document.createElement("h4");
+        teams_count.innerHTML = (t_t.teams.length < 2) ? `${t_t.teams.length} Team` :  `${t_t.teams.length} Teams`;
+        first_half.appendChild(teams_count);
+
+    information_div.appendChild(first_half);
+
+    t_t.teams.forEach((element, index) => {
+        let parent_div = document.createElement("div");
+            parent_div.classList.add("tournament_team_list_element");
+
+        let icon = document.createElement("img")
+            icon.src = element.participant.attached_participatable_portrait_url;
+            parent_div.appendChild(icon);
+
+        let name = document.createElement("h2");
+            name.innerHTML = element.participant.name;
+            parent_div.appendChild(name);
+
+        information_div.appendChild(parent_div);
+    });
+
+    $('#tournament_matchups').challonge(t_t.information.url, {subdomain: '', theme: '2', multiplier: '1.0', match_width_multiplier: '1.0', show_final_results: '0', show_standings: '0'});
+
+    document.getElementById("tournament_info").append(information_div);
+};
+
+const loadData = () => {
+    // Load in Data
+    let tournament_response = $.get("https://api.challonge.com/v1/tournaments.json", { api_key: KEY } )
+    .done(function() {
+        let tourney = JSON.parse(tournament_response.responseText);
+
+        tourney.forEach((element, index) => {
+            let rt = JSON.parse(tournament_response.responseText)[index].tournament;
+            
+            let teams_response = $.get(`https://api.challonge.com/v1/tournaments/${rt.id}/participants.json`, { api_key: KEY } )
+            .done(function() {
+                console.log(JSON.parse(teams_response.responseText));
+
+                createTournament(rt, JSON.parse(teams_response.responseText));
+                tm('#tournaments').displayTournaments();
+                tm('#teams').displayTeams();
+            });
+        });
+    });  
+}
+  
+tm().login();
